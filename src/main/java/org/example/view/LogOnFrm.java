5859a6d15285228d4170160f859159829f9610fb
PlutoCtx <chentingxian195467@163.com>
2023-07-02 17:07:37 +0800

    simplify LogOnFrm

--- src/main/java/org/example/view/LogOnFrm.java
+++ src/main/java/org/example/view/LogOnFrm.java
@@ -9,6 +9,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.sql.Connection;
 import java.util.Objects;
+import java.util.Enumeration;
+
 
 /**
  * CateringSystem
@@ -18,17 +20,19 @@ import java.util.Objects;
  * @since JDK17
  */
 
-public class LogOnFrm  extends javax.swing.JFrame {
+public class LogOnFrm  extends JFrame {
 
     DBUtil dbUtil = new DBUtil();
     UserDao userDao = new UserDao();
     public static User s_currentUser = null;//保存登陆用户的参数
+    private JPasswordField passwordTxt;
+    private JTextField userNameTxt;
 
     /** Creates new form LogOnFrm */
     public LogOnFrm() {
         //改变系统默认字体
         Font font = new Font("Dialog", Font.PLAIN, 12);
-        java.util.Enumeration keys = UIManager.getDefaults().keys();
+        Enumeration keys = UIManager.getDefaults().keys();
         while (keys.hasMoreElements()) {
             Object key = keys.nextElement();
             Object value = UIManager.get(key);
@@ -41,177 +45,107 @@ public class LogOnFrm  extends javax.swing.JFrame {
         this.setLocationRelativeTo(null);
     }
 
-    //GEN-BEGIN:initComponents
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">
+
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jb_logon = new javax.swing.JButton();
-        jb_register = new javax.swing.JButton();
-        userNameTxt = new javax.swing.JTextField();
-        passwordTxt = new javax.swing.JPasswordField();
-        jb_reset = new javax.swing.JButton();
+        JLabel showLabel = new JLabel();
+        JLabel usernameLabel = new JLabel();
+        JLabel passwordLabel = new JLabel();
+        JButton loginBtn = new JButton();
+        JButton registerBtn = new JButton();
+        userNameTxt = new JTextField();
+        passwordTxt = new JPasswordField();
+        JButton resetBtn = new JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("系统");
         setResizable(false);
 
-        jLabel1.setFont(new java.awt.Font("宋体", 1, 24));
-        jLabel1.setIcon(new javax.swing.ImageIcon(Objects.requireNonNull(getClass().getResource("/OrderMeal.png")))); // NOI18N
-        jLabel1.setText("餐饮系统欢迎");
+        showLabel.setFont(new java.awt.Font("宋体", 1, 24));
+        showLabel.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/OrderMeal.png"))));
+        showLabel.setText("餐饮系统欢迎");
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(Objects.requireNonNull(getClass().getResource("/userName.png")))); // NOI18N
-        jLabel2.setText("用户名：");
+        usernameLabel.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/userName.png"))));
+        usernameLabel.setText("用户名：");
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(Objects.requireNonNull(getClass().getResource("/password.png")))); // NOI18N
-        jLabel3.setText("密  码：");
+        passwordLabel.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/password.png"))));
+        passwordLabel.setText("密  码：");
 
-        jb_logon.setIcon(new javax.swing.ImageIcon(Objects.requireNonNull(getClass().getResource("/login.png")))); // NOI18N
-        jb_logon.setText("登陆");
-        jb_logon.addActionListener(this::jb_logonActionPerformed);
+        loginBtn.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/login.png"))));
+        loginBtn.setText("登陆");
+        loginBtn.addActionListener(this::loginBtnActionPerformed);
 
-        jb_register.setIcon(new javax.swing.ImageIcon(Objects.requireNonNull(getClass().getResource("/register.png")))); // NOI18N
-        jb_register.setText("注册用户");
-        jb_register.addActionListener(this::jb_registerActionPerformed);
+        registerBtn.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/register.png"))));
+        registerBtn.setText("注册用户");
+        registerBtn.addActionListener(this::registerBtnActionPerformed);
 
-        jb_reset.setIcon(new javax.swing.ImageIcon(Objects.requireNonNull(getClass().getResource("/reset.png")))); // NOI18N
-        jb_reset.setText("重置");
-        jb_reset.addActionListener(this::jb_resetActionPerformed);
+        resetBtn.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/reset.png"))));
+        resetBtn.setText("重置");
+        resetBtn.addActionListener(this::resetBtnActionPerformed);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
-                getContentPane());
+        GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
-        layout
-                .setHorizontalGroup(layout
-                        .createParallelGroup(
-                                javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(
-                                javax.swing.GroupLayout.Alignment.TRAILING,
-                                layout
-                                        .createSequentialGroup()
-                                        .addGap(52, 52, 52)
-                                        .addGroup(
-                                                layout
-                                                        .createParallelGroup(
-                                                                javax.swing.GroupLayout.Alignment.LEADING)
-                                                        .addGroup(
-                                                                layout
-                                                                        .createSequentialGroup()
-                                                                        .addGroup(
-                                                                                layout
-                                                                                        .createParallelGroup(
-                                                                                                javax.swing.GroupLayout.Alignment.LEADING)
-                                                                                        .addComponent(
-                                                                                                jLabel2)
-                                                                                        .addComponent(
-                                                                                                jLabel3))
-                                                                        .addGap(
-                                                                                33,
-                                                                                33,
-                                                                                33)
-                                                                        .addGroup(
-                                                                                layout
-                                                                                        .createParallelGroup(
-                                                                                                javax.swing.GroupLayout.Alignment.LEADING,
-                                                                                                false)
-                                                                                        .addComponent(
-                                                                                                passwordTxt)
-                                                                                        .addComponent(
-                                                                                                userNameTxt,
-                                                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                                                                193,
-                                                                                                Short.MAX_VALUE)))
-                                                        .addGroup(
-                                                                layout
-                                                                        .createParallelGroup(
-                                                                                javax.swing.GroupLayout.Alignment.TRAILING)
-                                                                        .addGroup(
-                                                                                javax.swing.GroupLayout.Alignment.LEADING,
-                                                                                layout
-                                                                                        .createSequentialGroup()
-                                                                                        .addGap(
-                                                                                                8,
-                                                                                                8,
-                                                                                                8)
-                                                                                        .addComponent(
-                                                                                                jb_logon)
-                                                                                        .addGap(
-                                                                                                18,
-                                                                                                18,
-                                                                                                18)
-                                                                                        .addComponent(
-                                                                                                jb_reset)
-                                                                                        .addGap(
-                                                                                                18,
-                                                                                                18,
-                                                                                                18)
-                                                                                        .addComponent(
-                                                                                                jb_register))
-                                                                        .addComponent(
-                                                                                jLabel1,
-                                                                                javax.swing.GroupLayout.PREFERRED_SIZE,
-                                                                                304,
-                                                                                javax.swing.GroupLayout.PREFERRED_SIZE)))
-                                        .addContainerGap(54, Short.MAX_VALUE)));
-        layout
-                .setVerticalGroup(layout
-                        .createParallelGroup(
-                                javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(
-                                layout
-                                        .createSequentialGroup()
-                                        .addGap(18, 18, 18)
-                                        .addComponent(
-                                                jLabel1,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE,
-                                                58,
-                                                javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addGap(27, 27, 27)
-                                        .addGroup(
-                                                layout
-                                                        .createParallelGroup(
-                                                                javax.swing.GroupLayout.Alignment.BASELINE)
-                                                        .addComponent(jLabel2)
-                                                        .addComponent(
-                                                                userNameTxt,
-                                                                javax.swing.GroupLayout.PREFERRED_SIZE,
-                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                                javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addGap(38, 38, 38)
-                                        .addGroup(
-                                                layout
-                                                        .createParallelGroup(
-                                                                javax.swing.GroupLayout.Alignment.BASELINE)
-                                                        .addComponent(jLabel3)
-                                                        .addComponent(
-                                                                passwordTxt,
-                                                                javax.swing.GroupLayout.PREFERRED_SIZE,
-                                                                javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                                javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addGap(37, 37, 37)
-                                        .addGroup(
-                                                layout
-                                                        .createParallelGroup(
-                                                                javax.swing.GroupLayout.Alignment.BASELINE)
-                                                        .addComponent(jb_logon)
-                                                        .addComponent(jb_reset)
-                                                        .addComponent(
-                                                                jb_register))
-                                        .addContainerGap(55, Short.MAX_VALUE)));
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(GroupLayout.Alignment.TRAILING,
+                layout.createSequentialGroup()
+                    .addGap(52, 52, 52)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                .addComponent(usernameLabel)
+                                .addComponent(passwordLabel))
+                            .addGap(33, 33, 33)
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                .addComponent(passwordTxt)
+                                .addComponent(userNameTxt, GroupLayout.DEFAULT_SIZE, 193, Short.MAX_VALUE)))
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                            .addGroup(GroupLayout.Alignment.LEADING,
+                                layout.createSequentialGroup()
+                                    .addGap(8, 8, 8)
+                                    .addComponent(loginBtn)
+                                    .addGap(18, 18, 18)
+                                    .addComponent(resetBtn)
+                                    .addGap(18, 18, 18)
+                                    .addComponent(registerBtn))
+                            .addComponent(showLabel, GroupLayout.PREFERRED_SIZE, 304, GroupLayout.PREFERRED_SIZE)))
+                    .addContainerGap(54, Short.MAX_VALUE)));
+        layout.setVerticalGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(showLabel, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(usernameLabel)
+                    .addComponent(
+                        userNameTxt,
+                        GroupLayout.PREFERRED_SIZE,
+                        GroupLayout.DEFAULT_SIZE,
+                        GroupLayout.PREFERRED_SIZE))
+                .addGap(38, 38, 38)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(passwordLabel)
+                    .addComponent(passwordTxt,
+                        GroupLayout.PREFERRED_SIZE,
+                        GroupLayout.DEFAULT_SIZE,
+                        GroupLayout.PREFERRED_SIZE))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(loginBtn)
+                    .addComponent(resetBtn)
+                    .addComponent(registerBtn))
+                .addContainerGap(55, Short.MAX_VALUE)));
 
         pack();
-    }// </editor-fold>
-    //GEN-END:initComponents
+    }
 
     //显示注册页面
-    private void jb_registerActionPerformed(java.awt.event.ActionEvent evt) {
+    private void registerBtnActionPerformed(java.awt.event.ActionEvent evt) {
         this.dispose();
         new RegisterFrm().setVisible(true);
     }
+
     //登陆验证显示登陆以后界面
-    private void jb_logonActionPerformed(java.awt.event.ActionEvent evt) {
+    private void loginBtnActionPerformed(java.awt.event.ActionEvent evt) {
         String userName = this.userNameTxt.getText();
         String password = new String(this.passwordTxt.getPassword());
         if (StringUtil.isEmpty(userName)) {
@@ -241,36 +175,22 @@ public class LogOnFrm  extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(null, "用户名或密码错误");
             }
         } catch (Exception e) {
-            
+
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, "用户名或密码错误");
         } finally {
             try {
                 dbUtil.closeCon(con);
             } catch (Exception e) {
-                
+
                 e.printStackTrace();
             }
         }
-
     }
     //重置文本框内容
-    private void jb_resetActionPerformed(java.awt.event.ActionEvent evt) {
+    private void resetBtnActionPerformed(java.awt.event.ActionEvent evt) {
         this.passwordTxt.setText("");
         this.userNameTxt.setText("");
     }
-
-
-    //GEN-BEGIN:variables
-    // Variables declaration - do not modify
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JButton jb_logon;
-    private javax.swing.JButton jb_register;
-    private javax.swing.JButton jb_reset;
-    private javax.swing.JPasswordField passwordTxt;
-    private javax.swing.JTextField userNameTxt;
-    // End of variables declaration//GEN-END:variables
 
 }
